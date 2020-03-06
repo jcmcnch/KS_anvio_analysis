ab8fec1da980e7a55deeb2d04fb26381fc694daa
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2020-03-05 19:51:07 -0800

    remove spaces

--- scripts/02-run-pangenome-workflow.sh
+++ scripts/02-run-pangenome-workflow.sh
@@ -3,7 +3,7 @@
 source activate anvio-6.1
 
 anvi-pan-genome -g Kueishan_Sulfurimonas-GENOMES.db \
-                --project-name "Kueishan Sulfurimonas Pangenome" \
+                --project-name "Kueishan_Sulfurimonas_Pangenome" \
                 --output-dir 01-KS_Sulfurimonas_pangenome_data \
                 --num-threads 1 \
                 --minbit 0.5 \
